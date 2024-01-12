6590204ac55f2a4ed1f8d218b29dbb60d311bceb
Joe Hawes <morehawes@gmail.com>
2024-01-12 07:55:58 -0800

    Moved app function to assets

--- assets/js/map-first.js
+++ assets/js/map-first.js
@@ -30,3 +30,115 @@ jQuery("document").ready(function () {
 			break;
 	}
 });
+
+/**
+ * Map Details Sidebar
+ *
+ * This function is called when a Waymark Shortcode has finished
+ * loading and has been provided this function name as a callback
+ * using the "loaded_callback" shortcode attribute.
+ * 
+ * It iterates over each Leaflet layer and creates an interactive
+ * list of overlays. Clicking on an overlay will open its popup
+ * and zoom to its location.
+ *
+ * It is passed a single argument - the Waymark_Instance object.
+ *
+ * Adapted From
+ *
+ * https://www.waymark.dev/docs/creating-an-interactive-overlay-list/
+ *
+ * Learn More
+ *
+ * https://www.waymark.dev/docs/callback-function/
+ * https://www.waymark.dev/docs/shortcodes/#callback-function
+ **/
+const map_first_single = (Waymark) => {
+  // Check for the Map First Sidebar
+  if (!jQuery(".map-first-sidebar").length) {
+		return false;
+	}
+
+	const container = jQuery(".map-first-sidebar").empty();
+  let overlays_content = jQuery(`<div />`).addClass("waymark-overlays waymark-accordion-container");
+
+  // Each Overlay Type
+  ["marker", "line", "shape"].forEach((type) => {
+    if (typeof Waymark[type + "_sub_groups"] === "object") {
+      //Get property keys
+      const type_keys = Object.keys(Waymark[type + "_sub_groups"]);
+
+      if (!type_keys.length) {
+        return false;
+      }
+
+      let type_content = jQuery(`<div />`)
+        .addClass("waymark-type waymark-accordion-group")
+        .append(jQuery(`<legend>${type.toUpperCase()}S</legend>`));
+
+      //Iterate over property keys
+      type_keys.forEach((key) => {
+        const group = Waymark[type + "_sub_groups"][key];
+
+        // Must be a LayerGroup
+        if (typeof group !== "object" || typeof group.getLayers !== "function") {
+          return false;
+        }
+
+        // Create group content
+        // Uses the very hand build_type_heading() helper function
+        let group_content = jQuery(`<div />`)
+          .addClass("waymark-group waymark-accordion-group-content")
+        	.append(Waymark.build_type_heading(type, key))
+        ;
+
+        //Iterate over sub groups
+        group.getLayers().forEach((layer) => {
+          if (typeof layer.feature !== "object") {
+            return;
+          }
+
+          // Create overlay content
+          let type_data = Waymark.get_type(type, layer.feature.properties.type);
+          // Uses the very hand build_overlay_content() helper function
+          let overlay_content = Waymark.build_overlay_content(layer.feature, type, type_data);
+
+          group_content.append(
+            jQuery("<div />")
+              .attr("href", "#")
+              .css("display", "block")
+              .html(overlay_content)
+              .addClass("waymark-overlay-content")
+              .on("click", (e) => {
+                e.preventDefault();
+
+                // Open popup
+                layer.openPopup();
+
+                switch (type) {
+                  case "marker":
+                    Waymark.map.setView(
+                      layer.getLatLng(),
+                      Waymark.map.getZoom() + 6,
+                    );
+                    break;
+                  case "line":
+                  case "shape":
+                    Waymark.map.fitBounds(layer.getBounds());
+                    break;
+                }
+              }),
+          );
+
+          type_content.append(group_content);
+        });
+
+        overlays_content.append(type_content);
+      }); // End iterate
+    }
+  });
+
+  
+
+  container.append(overlays_content);
+}
